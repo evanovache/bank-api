265c17e6383aec4148067ebdae42c45082195e03
Evans Keborni <evans.keborni@etranzact.com.gh>
2026-01-21 21:38:01 +0000

    Organize imports

--- src/main/java/com/etz/controller/AccountResource.java
+++ src/main/java/com/etz/controller/AccountResource.java
@@ -11,7 +11,6 @@ import com.etz.service.BankService;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
